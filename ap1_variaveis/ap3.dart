f226f7e4225be6eeda2d7bc16a8cfbed8f886216
Arthur Furtado <arthur.furtado@unifebe.edu.br>
2026-05-29 10:15:14 -0300

    ap3 completa

---
+++ ap1_variaveis/ap3.dart
@@ -0,0 +1,18 @@
+import 'dart:math';
+
+void main() {
+  final random = Random();
+  var a = random.nextInt(100) + 1; // gera entre 1 e 100
+  var b = random.nextInt(100) + 1;
+  var temp = a; // variável temporária para guardar 'a' antes de perder o valor original
+
+  // print antes de inverter os valores
+  print("Antes: a = ${a} , b = ${b}");
+
+  // inversão dos valores
+  a = b;
+  b = temp;
+
+  // print dos valores invertidos
+  print("Depois: a = ${a} , b = ${b}");
+}
