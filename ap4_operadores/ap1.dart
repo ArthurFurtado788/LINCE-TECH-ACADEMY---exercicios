54f40b06238b152221cf06ef4dda15e277f606ba
Arthur Furtado <arthur.furtado@unifebe.edu.br>
2026-06-29 19:13:02 -0300

    ap4 - ap1 completa

---
+++ ap4_operadores/ap1.dart
@@ -0,0 +1,15 @@
+bool ehAnoBissexto(int ano) {
+   return (ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0);
+}
+
+void main() {
+   List<int> anos = [2016, 1988, 2000, 2100, 2300, 1993];
+ 
+  for (int ano in anos) {
+    if (ehAnoBissexto(ano)) {
+      print('O ano $ano é bissexto');
+    } else {
+      print('O ano $ano nao é bissexto');
+    }
+  }
+}
